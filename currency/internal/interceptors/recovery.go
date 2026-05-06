a6fdb84836ac9ccfac0395909e52551b3c145582
Unheilo <Mincalm@gmail.com>
2026-05-07 09:24:24 +1000

    working on middleware

---
+++ currency/internal/interceptors/recovery.go
@@ -0,0 +1,30 @@
+package interceptors
+
+import (
+	"context"
+	"log/slog"
+	"runtime/debug"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func Recovery(log *slog.Logger) grpc.UnaryServerInterceptor {
+	return func(
+		ctx context.Context, req interface{},
+		info *grpc.UnaryServerInfo, handler grpc.UnaryHandler,
+	) (resp interface{}, err error) {
+		defer func() {
+			if r := recover(); r != nil {
+				log.LogAttrs(ctx, slog.LevelError, "panic recovered",
+					slog.String("method", info.FullMethod),
+					slog.Any("panic", r),
+					slog.String("stack", string(debug.Stack())),
+				)
+				err = status.Errorf(codes.Internal, "internal server error")
+			}
+		}()
+		return handler(ctx, req)
+	}
+}
---
+++ currency/internal/interceptors/request_id.go
@@ -0,0 +1,41 @@
+package interceptors
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
+)
+
+type ctxKey struct{ name string }
+
+var RequestIDKey = ctxKey{"request_id"}
+
+const MDHeaderRequestID = "x-request-id"
+
+func RequestID() grpc.UnaryServerInterceptor {
+	return func(
+		ctx context.Context, req interface{},
+		info *grpc.UnaryServerInfo, handler grpc.UnaryHandler,
+	) (interface{}, error) {
+		id := ""
+		if md, ok := metadata.FromIncomingContext(ctx); ok {
+			if vals := md.Get(MDHeaderRequestID); len(vals) > 0 {
+				id = vals[0]
+			}
+		}
+		if id == "" {
+			id = uuid.NewString()
+		}
+		ctx = context.WithValue(ctx, RequestIDKey, id)
+		return handler(ctx, req)
+	}
+}
+
+func RequestIDFromContext(ctx context.Context) string {
+	if v, ok := ctx.Value(RequestIDKey).(string); ok {
+		return v
+	}
+	return ""
+}
